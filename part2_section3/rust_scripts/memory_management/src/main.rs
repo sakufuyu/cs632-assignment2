9b0479816f8aafc31e30c946c3237d9c58ca63e8
sakufuyu <kobafuyu@gmail.com>
2025-09-05 13:02:02 -0700

    Assignment 2 - Part 2 - Test String.clone() method

--- part2_section3/rust_scripts/memory_management/src/main.rs
+++ part2_section3/rust_scripts/memory_management/src/main.rs
@@ -6,8 +6,8 @@ fn string_lenght(ss: String) -> usize {
 fn main() {
     println!("Rust's ownership checking.");
     let s = String::from("Hello");
-    let len = string_lenght(s);
+    let len = string_lenght(s.clone());
     // From here s is no longer valid
-    // println!("The length of '{}' is {}.", s, len); // Error
-    println!("The length of '{}' is {}.", "Hello", len);
+    println!("The length of '{}' is {}.", s, len); // Error
+    // println!("The length of '{}' is {}.", "Hello", len);
 }
