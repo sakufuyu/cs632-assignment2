9e65c57ac0aeccff2be9e21ded86fc64de34e928
sakufuyu <kobafuyu@gmail.com>
2025-09-05 13:05:24 -0700

    Assignment 2 - Part 2 - Test borrowing method

--- part2_section3/rust_scripts/memory_management/src/main.rs
+++ part2_section3/rust_scripts/memory_management/src/main.rs
@@ -1,12 +1,12 @@
 
-fn string_lenght(ss: String) -> usize {
+fn string_lenght(ss: &String) -> usize {
     ss.len()
 }
 
 fn main() {
     println!("Rust's ownership checking.");
     let s = String::from("Hello");
-    let len = string_lenght(s.clone());
+    let len = string_lenght(&s);
     // From here s is no longer valid
     println!("The length of '{}' is {}.", s, len); // Error
     // println!("The length of '{}' is {}.", "Hello", len);
